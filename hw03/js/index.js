f3e051cfe9d2cb35893b7bdaf28e6b95f4019264
Spencer <spencerlevitt2022@u.northwestern.edu>
2021-05-12 19:31:06 -0500

    Checking in my hw03 folder

---
+++ hw03/js/index.js
@@ -0,0 +1,84 @@
+/**
+ *
+ * -------------------------------------
+ * DOM Manipulation / Traversal Activity
+ * -------------------------------------
+ *
+ * 1. Create and attach an event handler (function) to each ".image"
+ * element so that when the ".image" element is clicked, the corresponding
+ * image loads in the .featured image element.
+ *
+ * 2. Create event handlers for the next and previous buttons. The next button should
+ *    show the next image in the thumbnail list. The previous should show the previous.
+ *
+ * 3. If you get to the end, start at the beginning.
+ *
+ * 4. If you get to the beginning, loop around to the end.
+ *
+ *
+ */
+
+const images = [
+  "images/field1.jpg",
+  "images/purple.jpg",
+  "images/jar.jpg",
+  "images/green.jpg",
+  "images/green1.jpg",
+  "images/purple1.jpg",
+  "images/magnolias.jpg",
+  "images/daisy1.jpg",
+];
+
+let currentIndex = 0;
+
+const initScreen = () => {
+  images.forEach((image, idx) => {
+    document.querySelector(".cards").innerHTML += `
+            <li class="card">
+                <div class="image" 
+                    style="background-image:url('${image}')"
+                    data-index="${idx}"></div>
+            </li>`;
+  });
+};
+
+// create event handler:
+const showImage = (ev) => {
+  const elem = ev.currentTarget;
+  currentIndex = parseInt(elem.dataset.index);
+  console.log("currentIndex:", currentIndex);
+  document.querySelector(".featured_image").style.backgroundImage =
+    elem.style.backgroundImage;
+};
+
+const showNext = (ev) => {
+  currentIndex =
+    currentIndex + 1 > images.length - 1
+      ? (currentIndex = 0)
+      : (currentIndex += 1);
+  console.log("currentIndex:", currentIndex);
+  document.querySelector(
+    ".featured_image"
+  ).style.backgroundImage = `url(${images[currentIndex]})`;
+};
+
+const showPrev = (ev) => {
+  currentIndex = currentIndex - 1 < 0 ? images.length - 1 : (currentIndex -= 1);
+  console.log("currentIndex:", currentIndex);
+  document.querySelector(
+    ".featured_image"
+  ).style.backgroundImage = `url(${images[currentIndex]})`;
+};
+
+initScreen();
+
+// attach event handler to all of the image tags
+// (after initScreen() has been invoked).
+const imageElements = document.querySelectorAll(".image");
+for (const elem of imageElements) {
+  elem.onclick = showImage;
+}
+
+document.querySelector(".next").onclick = showNext;
+document.querySelector(".prev").onclick = showPrev;
+document.querySelector(".featured_image").onclick = showNext;
